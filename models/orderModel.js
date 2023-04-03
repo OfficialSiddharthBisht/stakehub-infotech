fed7b3e53dd990fc60a58727a7607e5fbbea9246
Siddharth <officialsiddharthbisht@gmail.com>
2023-04-03 20:53:58 +0530

    feat: order model

---
+++ models/orderModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    buyerQty: {
+        type: Number,
+        required: true,
+    },
+    buyerPrice: {
+        type: Number,
+        required: true,
+    },
+    sellerPrice: {
+        type: Number,
+        required: true,
+    },
+    sellerQty: {
+        type: Number,
+        required: true,
+    },
+})
+
+module.exports = mongoose.model("Order", orderSchema);
